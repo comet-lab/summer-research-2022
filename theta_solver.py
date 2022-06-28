5de4f20bf70c70c74d330c86942d6117a9e02ff0
mlg2709 <107885805+mlg2709@users.noreply.github.com>
2022-06-28 12:19:37 -0400

    Update theta_solver.py

--- theta_solver.py
+++ theta_solver.py
@@ -15,7 +15,7 @@ def find_kappas(n, theta_array, h_array, y_bar_array):
 def find_epsilons(n, kappa_array, r_outer, y_bar_array):
     epsilon_array = np.array([])
     for j in range(0, n):
-        epsilon = (kappa_array[j]*(r_outer - y_bar_array[j]))/(1 + (y_bar_array[j]*kappa_array[j])                )
+        epsilon = (kappa_array[j]*(r_outer - y_bar_array[j]))/(1 + (y_bar_array[j]*kappa_array[j]))
         epsilon_array = np.append(epsilon_array, epsilon)
     return epsilon_array
 
@@ -48,63 +48,54 @@ def update_Es(n, eta, previous_E_array, theta_array, h_array, y_bar_array, r_out
         E_array = np.append(E_array, E)
     return E_array
 
-# equation 2 in Swaney & York 2016
-def find_phi_outers(n, g_array, r_outer):
-    phi_outer_array = np.array([])
+# equation A.7 Masters Thesis
+def find_phi(n, g_array, r_outer):
+    phi_array = np.array([])
     for j in range(0, n):
-        phi_outer  = 2*np.arccos((g_array[j]-r_outer)/r_outer)
-        phi_outer_array = np.append(phi_outer_array, phi_outer)
-    return phi_outer_array
+        phi = 2*np.arccos((g_array[j]-r_outer)/r_outer)
+        phi_array = np.append(phi_array, phi)
+    return phi_array
 
-# equation 2 in Swaney & York 2016       
-def find_phi_inners(n, g_array, r_outer, r_inner):
-    phi_inner_array = np.array([])
-    for j in range(0, n):
-        phi_inner  = 2*np.arccos((g_array[j]-r_outer)/r_inner)
-        phi_inner_array = np.append(phi_inner_array, phi_inner)
-    return phi_inner_array
-
-# equation 2 in Swaney & York 2016
-def find_A_outers(n, r_outer, phi_outer_array):
+# equation A.7 Masters Thesis
+def find_A_outers(n, r_outer, phi_array):
     A_outer_array = np.array([])
     for j in range(0, n):
-        A_outer = r_outer**2 * ((phi_outer_array[j] - np.sin(phi_outer_array[j]))/2)
+        A_outer = r_outer**2 * (phi_array[j]/2)
         A_outer_array = np.append(A_outer_array, A_outer)
     return A_outer_array
 
-# equation 2 in Swaney & York 2016
-def find_A_inners(n, r_inner, phi_inner_array):
+# equation A.7 Masters Thesis
+def find_A_inners(n, r_inner, phi_array):
     A_inner_array = np.array([])
     for j in range(0, n):
-        A_inner = r_inner**2 * ((phi_inner_array[j] - np.sin(phi_inner_array[j]))/2)
+        A_inner = r_inner**2 * (phi_array[j]/2)
         A_inner_array = np.append(A_inner_array, A_inner)
     return A_inner_array
 
-# equation 2 in Swaney & York 2016
-def find_y_bar_os(n, r_outer, phi_outer_array):
+# equation A.7 Masters Thesis 
+def find_y_bar_os(n, r_outer, phi_array):
     y_bar_o_array = np.array([])
     for j in range(0, n):
-        y_bar_o = (4*r_outer*(np.sin(.5*phi_outer_array[j])**3))/(3*(phi_outer_array[j]-np.sin(phi_outer_array[j])))
+        y_bar_o = (2*r_outer*math.sin(phi_array[j]/2))/(3*phi_array[j]/2)
         y_bar_o_array = np.append(y_bar_o_array, y_bar_o)
     return y_bar_o_array
 
-# equation 2 in Swaney & York 2016
-def find_y_bar_is(n, r_inner, phi_inner_array):
+# equation A.7 Masters Thesis 
+def find_y_bar_is(n, r_inner, phi_array):
     y_bar_i_array = np.array([])
     for j in range(0, n):
-        y_bar_i = (4*r_inner*(np.sin(.5*phi_inner_array[j])**3))/(3*(phi_inner_array[j]-np.sin(phi_inner_array[j])))
+        y_bar_i = (2*r_inner*math.sin(phi_array[j]/2))/(3*phi_array[j]/2)
         y_bar_i_array = np.append(y_bar_i_array, y_bar_i)
     return y_bar_i_array
 
 
 # equation 1 in Swaney & York 2016
 def find_y_bars(n, r_outer, r_inner, g_array):
-    phi_outer_array = find_phi_outers(n, g_array, r_outer)
-    phi_inner_array = find_phi_inners(n, g_array, r_outer, r_inner)
-    y_bar_o_array = find_y_bar_os(n, r_outer, phi_outer_array)
-    y_bar_i_array = find_y_bar_is(n, r_inner, phi_inner_array)
-    A_outer_array = find_A_outers(n, r_outer, phi_outer_array)
-    A_inner_array = find_A_inners(n, r_inner, phi_inner_array)
+    phi_array = find_phi(n, g_array, r_outer)
+    y_bar_o_array = find_y_bar_os(n, r_outer, phi_array)
+    y_bar_i_array = find_y_bar_is(n, r_inner, phi_array)
+    A_outer_array = find_A_outers(n, r_outer, phi_array)
+    A_inner_array = find_A_inners(n, r_inner, phi_array)
     
     y_bar_array = np.array([])
     for j in range(0, n):
@@ -112,11 +103,12 @@ def find_y_bars(n, r_outer, r_inner, g_array):
         y_bar_array = np.append(y_bar_array, y_bar)
     return y_bar_array
 
-# 1/5 the second moment of the area for an annulus (https://en.wikipedia.org/wiki/Second_moment_of_area)
-def find_Is(n, r_outer, r_inner):
+# https://www.efunda.com/math/areas/CircularSection.cfm
+def find_Is(n, r_outer, r_inner, g_array):
+    phi_array = find_phi(n, g_array, r_outer)
     I_array = np.array([])
     for j in range(0, n):
-        I = (math.pi/10)*(r_outer**4 - r_inner**4)
+        I = ((phi_array[j]/2) + .5*math.sin(phi_array[j])) * (((r_outer **4) /4 )- ((r_inner **4) /4 ))
         I_array = np.append(I_array, I)
     return I_array
 
@@ -161,22 +153,25 @@ def solve_for_thetas(n, y_bar_array, h_array, I_array, mu, r_inner, r_outer, Fp,
 #test values
 n = 5 # number of notches
 mu = 0.13 # coefficient of friction
-eta = 0.05 # gradient descent rate (guess)
+eta = 0.2 # gradient descent rate (guess)
 E_super = 3e9 # Pascals table 3 (given in giga Pascals)
 epsilon_low = 0.028 # table 3
-sigma_low = 5e8 # figure 6
-E_linear = sigma_low/epsilon_low # to be consistent with epsilon_low and sigma_low
-r_outer = 0.0009 #millimeters (guess)
-r_inner = 0.0007 # millimeters (guess)
+E_linear = 1e10  # table 3
+sigma_low = E_linear*epsilon_low 
+r_outer = 0.00081 #millimeters (table 2)
+r_inner = 0.0007 # millimeters (table 2)
 h_array = np.array([0.0008, 0.0008, 0.0008, 0.0008, 0.0008]) # table 2
 g_array = np.array([0.0014, 0.0014, 0.0014, 0.0014, 0.0014]) #table 2
 y_bar_array = find_y_bars(n, r_outer, r_inner, g_array) 
-I_array = find_Is(n, r_outer, r_inner) # 1/5 of the second moment of area for an annulus 
-Fp = 500 #Newtons (guess)
+I_array = find_Is(n, r_outer, r_inner, g_array) 
+Fp = 130 #Newtons (figure 9)
 
-
+print('y_bar_array', y_bar_array)
 print('final theta_array', solve_for_thetas(n, y_bar_array, h_array, I_array, mu, r_inner, r_outer, Fp, eta, E_linear, E_super, epsilon_low, sigma_low))
 
 
-# test result: theta_array = [0.73846696 0.63541634 0.55247992 0.48449348 0.42790712]
-# final E_array values = [5.28022990e+09 5.65003325e+09 6.04784730e+09 6.47553580e+09 6.93514007e+09]
+# test results:
+#y_bar_array [0.00068653 0.00068653 0.00068653 0.00068653 0.00068653]
+#initial E_array [1.e+10 1.e+10 1.e+10 1.e+10 1.e+10]
+#final E_array [6.66350434e+09 7.02278977e+09 7.40211304e+09 7.80254663e+09 8.22522662e+09]
+#final theta_array [0.34665659 0.31569582 0.2884928  0.26443844 0.24304743]
