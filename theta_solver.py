89ee3d7dfbc809af2eae0f8df4208564d490e527
mlg2709 <107885805+mlg2709@users.noreply.github.com>
2022-07-01 13:08:05 -0400

    Update theta_solver.py

--- theta_solver.py
+++ theta_solver.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.optimize as sp
 import math
+import matplotlib.pyplot as plt
 
 
 #equation 15 in N. Pacheco 2021
@@ -40,7 +41,7 @@ def update_Es(n, eta, previous_E_array, theta_array, h_array, y_bar_array, r_out
     E_array = np.array([])
     
     for j in range(0, n):        
-        if epsilon_array[j] == 0:
+        if epsilon_array[j] == 0: 
             E = previous_E_array[j] - eta*(previous_E_array[j] - E_linear)
         else:
             E = previous_E_array[j] - eta*(previous_E_array[j] - (sigma_array[j]/epsilon_array[j]))
@@ -104,14 +105,25 @@ def find_y_bars(n, r_outer, r_inner, g_array):
     return y_bar_array
 
 # https://www.efunda.com/math/areas/CircularSection.cfm
-def find_Is(n, r_outer, r_inner, g_array):
+# https://www.efunda.com/math/areas/ParallelAxisTheorem.cfm
+def find_Is(n, r_outer, r_inner, g_array, y_bar_array):
     phi_array = find_phi(n, g_array, r_outer)
+    A_outer_array = find_A_outers(n, r_outer, phi_array)
+    A_inner_array = find_A_inners(n, r_inner, phi_array)
     I_array = np.array([])
     for j in range(0, n):
-        I = ((phi_array[j]/2) + .5*math.sin(phi_array[j])) * (((r_outer **4) /4 )- ((r_inner **4) /4 ))
+        I = (((phi_array[j]/2) + .5*math.sin(phi_array[j])) * (((r_outer **4) /4 )- ((r_inner **4) /4 ))
+               - y_bar_array[j]**2 * (A_outer_array[j] - A_inner_array[j]))
         I_array = np.append(I_array, I)
     return I_array
 
+# equation 8 York & Swaney 2015
+def find_theta_max(n, h_array, r_outer, y_bar_array):
+    theta_max_array = np.array([])
+    for j in range(0, n):
+        theta_max = h_array[j]/(r_outer + y_bar_array[j])
+        theta_max_array = np.append(theta_max_array, theta_max)
+    return theta_max_array
 
 # f(theta) = theta - D*exp(-mu*theta)
 def key_function(x, D, mu):
@@ -123,34 +135,39 @@ def key_function_prime(x, D, mu):
     y_prime = 1 + mu*D*math.exp(-mu*x)
     return y_prime
 
+
 #used Newton's method to solve equation 12 in N. Pacheco 2021
 initial_value = 0.0
-def find_thetas(n, y_bar_array, h_array, E_array, I_array, mu, r_inner, Fp):
+def find_thetas(n, y_bar_array, h_array, E_array, I_array, mu, r_inner, Fp, theta_max_array):
     theta_array = np.array([])
     for j in range(0, n):
        D = (((r_inner + y_bar_array[j])* h_array[j]*Fp)/(E_array[j]*I_array[j]) )* math.exp(-mu * theta_array.sum())
        theta = sp.newton(key_function, initial_value, fprime=key_function_prime, args=(D, mu))
+       if theta > theta_max_array[j]:
+           theta = theta_max_array[j]
        theta_array = np.append(theta_array, theta)
     return theta_array
 
-
 #iterative process from section 3.3 in N. Pacheco 2021
-def solve_for_thetas(n, y_bar_array, h_array, I_array, mu, r_inner, r_outer, Fp, eta, E_linear, E_super, epsilon_low, sigma_low):
+def solve_for_thetas(n, y_bar_array, h_array, I_array, theta_max_array, mu, r_inner, r_outer, Fp, eta, E_linear, E_super, epsilon_low, sigma_low):
     E_array = E_linear * np.ones(n)
-    print('initial E_array', E_array)
-    theta_array = find_thetas(n, y_bar_array, h_array, E_array, I_array, mu, r_inner, Fp)
+    #print('initial E_array', E_array)
+    theta_array = find_thetas(n, y_bar_array, h_array, E_array, I_array, mu, r_inner, Fp, theta_max_array)
     previous_E_array = E_array
     E_array = update_Es(n, eta, previous_E_array, theta_array, h_array, y_bar_array, r_outer, E_linear, E_super, epsilon_low, sigma_low)
+
     
-    while np.min(np.absolute(E_array - previous_E_array)) >= 10**-4:
-        theta_array = find_thetas(n, y_bar_array, h_array, E_array, I_array, mu, r_inner, Fp)
+    while np.max(np.absolute(E_array - previous_E_array)) >= 1e-4:
+        theta_array = find_thetas(n, y_bar_array, h_array, E_array, I_array, mu, r_inner, Fp, theta_max_array)
         previous_E_array = E_array
         E_array = update_Es(n, eta, previous_E_array, theta_array, h_array, y_bar_array, r_outer, E_linear, E_super, epsilon_low, sigma_low)
-
-    print('final E_array', E_array)
+    #print('final E_array', E_array)
     return theta_array
 
-#test values
+
+
+
+#test values for wrist A 
 n = 5 # number of notches
 mu = 0.13 # coefficient of friction
 eta = 0.2 # gradient descent rate (guess)
@@ -163,11 +180,12 @@ r_inner = 0.0007 # millimeters (table 2)
 h_array = np.array([0.0008, 0.0008, 0.0008, 0.0008, 0.0008]) # table 2
 g_array = np.array([0.0014, 0.0014, 0.0014, 0.0014, 0.0014]) #table 2
 y_bar_array = find_y_bars(n, r_outer, r_inner, g_array) 
-I_array = find_Is(n, r_outer, r_inner, g_array) 
-Fp = 130 #Newtons (figure 9)
+I_array = find_Is(n, r_outer, r_inner, g_array, y_bar_array)
+theta_max_array = find_theta_max(n, h_array, r_outer, y_bar_array)
+Fp = 1.5 #Newtons (figure 9)
 
-print('y_bar_array', y_bar_array)
-print('final theta_array', solve_for_thetas(n, y_bar_array, h_array, I_array, mu, r_inner, r_outer, Fp, eta, E_linear, E_super, epsilon_low, sigma_low))
+#print('y_bar_array', y_bar_array)
+#print('final theta_array', solve_for_thetas(n, y_bar_array, h_array, I_array, mu, r_inner, r_outer, Fp, eta, E_linear, E_super, epsilon_low, sigma_low))
 
 
 # test results:
@@ -175,3 +193,144 @@ print('final theta_array', solve_for_thetas(n, y_bar_array, h_array, I_array, mu
 #initial E_array [1.e+10 1.e+10 1.e+10 1.e+10 1.e+10]
 #final E_array [6.66350434e+09 7.02278977e+09 7.40211304e+09 7.80254663e+09 8.22522662e+09]
 #final theta_array [0.34665659 0.31569582 0.2884928  0.26443844 0.24304743]
+
+
+
+
+
+
+
+
+forces = np.arange(0, 2.6, 0.05)
+deflections = np.ones((52, 5))
+
+
+for k in range(0, len(forces)):
+    theta_array = solve_for_thetas(n, y_bar_array, h_array, I_array, theta_max_array, mu, r_inner, r_outer, forces[k], eta, E_linear, E_super, epsilon_low, sigma_low)
+    deflections[k] = theta_array
+        
+#print(deflections)
+
+#print(deflections.shape)
+
+figure, axis = plt.subplots(1, 3)
+axis[0].set_title('Force Model - Wrist A')
+axis[0].set_xlabel('Force')
+axis[0].set_ylabel('Deflection')
+
+
+colors = ['orange', 'green', 'blue', 'red', 'aqua']
+
+deflections = deflections.transpose()
+
+#print(deflections)
+
+
+
+for k in range(0, len(deflections)):
+    axis[0].plot(forces, deflections[k]*(180/math.pi), color=colors[k])
+
+
+
+
+
+#test values for wrist B 
+n = 5 # number of notches
+mu = 0.13 # coefficient of friction
+eta = 0.2 # gradient descent rate (guess)
+E_super = 3e9 # Pascals table 3 (given in giga Pascals)
+epsilon_low = 0.028 # table 3
+E_linear = 1e10  # table 3
+sigma_low = E_linear*epsilon_low 
+r_outer = 0.00081 #millimeters (table 2)
+r_inner = 0.0007 # millimeters (table 2)
+h_array = np.array([0.0005, 0.0005, 0.0005, 0.0005, 0.0005]) # table 2
+g_array = np.array([0.0014, 0.0014, 0.0014, 0.0014, 0.0014]) #table 2
+y_bar_array = find_y_bars(n, r_outer, r_inner, g_array) 
+I_array = find_Is(n, r_outer, r_inner, g_array, y_bar_array)
+theta_max_array = find_theta_max(n, h_array, r_outer, y_bar_array)
+Fp = 1.5 #Newtons (figure 9)
+
+
+
+forces = np.arange(0, 2.6, 0.05)
+deflections = np.ones((52, n))
+
+
+for k in range(0, len(forces)):
+    theta_array = solve_for_thetas(n, y_bar_array, h_array, I_array, theta_max_array, mu, r_inner, r_outer, forces[k], eta, E_linear, E_super, epsilon_low, sigma_low)
+    deflections[k] = theta_array
+        
+#print(deflections)
+
+#print(deflections.shape)
+
+
+
+axis[1].set_title('Force Model - Wrist B')
+axis[1].set_xlabel('Force')
+axis[1].set_ylabel('Deflection')
+
+
+colors = ['orange', 'green', 'blue', 'red', 'aqua']
+
+deflections = deflections.transpose()
+
+#print(deflections)
+
+
+
+for k in range(0, len(deflections)):
+    axis[1].plot(forces, deflections[k]*(180/math.pi), color=colors[k])
+
+
+
+#test values for wrist C
+n = 4 # number of notches
+mu = 0.13 # coefficient of friction
+eta = 0.2 # gradient descent rate (guess)
+E_super = 3e9 # Pascals table 3 (given in giga Pascals)
+epsilon_low = 0.028 # table 3
+E_linear = 1e10  # table 3
+sigma_low = E_linear*epsilon_low 
+r_outer = 0.00081 #millimeters (table 2)
+r_inner = 0.0007 # millimeters (table 2)
+h_array = np.array([0.001, 0.001, 0.001, 0.001]) # table 2
+g_array = np.array([0.00136, 0.00139, 0.00142, 0.00145]) #table 2
+y_bar_array = find_y_bars(n, r_outer, r_inner, g_array) 
+I_array = find_Is(n, r_outer, r_inner, g_array, y_bar_array)
+theta_max_array = find_theta_max(n, h_array, r_outer, y_bar_array)
+Fp = 1.5 #Newtons (figure 9)
+
+
+forces = np.arange(0, 2.6, 0.05)
+deflections = np.ones((52, 4))
+
+
+for k in range(0, len(forces)):
+    theta_array = solve_for_thetas(n, y_bar_array, h_array, I_array, theta_max_array, mu, r_inner, r_outer, forces[k], eta, E_linear, E_super, epsilon_low, sigma_low)
+    deflections[k] = theta_array
+        
+#print(deflections)
+
+#print(deflections.shape)
+
+axis[2].set_title('Force Model - Wrist C')
+axis[2].set_xlabel('Force')
+axis[2].set_ylabel('Deflection')
+
+
+colors = ['orange', 'green', 'blue', 'red']
+
+deflections = deflections.transpose()
+
+#print(deflections)
+
+
+
+for k in range(0, len(deflections)):
+    axis[2].plot(forces, deflections[k]*(180/math.pi), color=colors[k])
+    
+plt.show()
+ 
+
